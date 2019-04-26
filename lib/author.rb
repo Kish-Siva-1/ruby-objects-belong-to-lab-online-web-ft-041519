a4e13531854ed4d2df1f065e6fc46d6db7276ec0
Kish Siva <rodes88@msn.com>
2019-04-26 00:32:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,10 +2,3 @@ class Author
   attr_accessor :name 
 end
 
-describe "Author" do 
-  it "has a name" do
-    author = Author.new
-    author.name = "Uncle Bob" 
-    expect(author.name).to eq("Uncle Bob")
-  end
-end
